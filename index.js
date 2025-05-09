98b0463c892537a6a04066872cfd26a632ff2a3c
Jaroly456 <jaroly.vidaldiaz25@compscihigh.org>
2025-05-09 14:27:03 +0000

    v0.1 beta

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const mongoose = require("mongoose");
+const express = require("express");
 
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+    console.log(`${req.method}: ${req.path}`);
+    next();
+});
