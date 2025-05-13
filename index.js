e6296b1dc7622a10e9b91b8d8ad755a899cb5d9a
Jaroly456 <jaroly.vidaldiaz25@compscihigh.org>
2025-05-13 15:12:16 +0000

    v0.2

--- index.js
+++ index.js
@@ -13,3 +13,25 @@ app.use((req, res, next) => {
     console.log(`${req.method}: ${req.path}`);
     next();
 });
+
+const rosterSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        image: {type: String},
+        grade: { type: Number, default: 0 },
+    }
+);
+
+const Memeber = mongoose.model("Member", rosterSchema, "Members")
+
+async function startServer() {
+    await mongoose.connect(
+      "mongodb+srv://SE12:CSH2025@cluster0.u9yhg.mongodb.net/CSHpets?retryWrites=true&w=majority&appName=Cluster0"
+    );
+  
+    app.listen(3000, () => {
+      console.log(`Server running.`);
+    });
+  }
+  
+  startServer();
